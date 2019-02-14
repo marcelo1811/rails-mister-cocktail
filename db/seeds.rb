3f4c6ce67aa7c5b4a721eb5f2bd16d306fc66dcc
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-14 12:35:04 -0200

    models validations and assocations added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts 'Seeding the database'
+
+# 1.upto(10) do
+
+# end
